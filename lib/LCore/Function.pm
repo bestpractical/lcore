d0b6398a904e157441d277c158202deddbbc3817
Chia-liang Kao <clkao@clkao.org>
2009-08-31 16:27:44 +0800

    use coercion for lcore::type.

--- lib/LCore/Function.pm
+++ lib/LCore/Function.pm
@@ -1,8 +1,19 @@
 package LCore::Function;
 use Moose::Role;
+use Moose::Util::TypeConstraints;
+
+type 'Function';
+
+subtype 'LCore::Type'
+    => as 'Object'
+    => where { $_->isa('Moose::Meta::TypeConstraint') };
+
+coerce 'LCore::Type'
+    => from 'Str'
+    => via { Moose::Util::TypeConstraints::find_or_create_isa_type_constraint( $_ ) };
 
 has parameters => (is => "ro", isa => "ArrayRef");
-has return_type => (is => "rw", isa => "Str");
+has return_type => (is => "rw", isa => "LCore::Type", coerce => 1);
 has lazy => (is => "ro", isa => "Bool", default => 1);
 has slurpy => (is => "ro", isa => "Bool", default => 0);
 
--- lib/LCore/Parameter.pm
+++ lib/LCore/Parameter.pm
@@ -1,19 +1,8 @@
 package LCore::Parameter;
 use Moose;
-use Moose::Util::TypeConstraints qw(find_type_constraint type);
-
-type 'Function';
 
 has name => (is => "ro", isa => "Str");
-has type => (is => "ro", isa => "Moose::Meta::TypeConstraint");
-
-sub BUILDARGS {
-    my ($self, $args) = @_;
-    if ($args->{type} && !ref($args->{type})) {
-        $args->{type} = Moose::Util::TypeConstraints::find_or_create_isa_type_constraint($args->{type})
-    }
-    return $args;
-}
+has type => (is => "ro", isa => "LCore::Type", coerce => 1);
 
 use overload (
     fallback => 1,
